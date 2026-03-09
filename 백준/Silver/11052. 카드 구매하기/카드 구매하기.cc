56e90a39da7b8fe80ac04bfd969895d39dcf9016
sooji123 <163733520+sooji123@users.noreply.github.com>
2026-03-09 21:28:03 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    
+    vector<int> p(n+1);
+    for(int i=1;i<=n;i++){
+        cin >> p[i];
+    }
+    
+    vector<int> dp(n+1,0);
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            dp[i] = max(dp[i],dp[i-j]+p[j]);
+        }
+    }
+    
+    cout << dp[n];
+}
