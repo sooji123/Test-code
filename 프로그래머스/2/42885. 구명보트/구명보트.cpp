aacf74fa4420b81c78713011034d00faecf81bff
sooji123 <163733520+sooji123@users.noreply.github.com>
2026-05-04 17:24:07 +0900

    [level 2] Title: 구명보트, Time: 1.79 ms, Memory: 4.67 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    
+    sort(people.begin(), people.end());
+    
+    int begin = 0;
+    int end = people.size()-1;
+    
+    
+    while(begin <= end){
+        if(people[begin]+people[end] <= limit){
+            begin++;
+        }
+        end--;
+        
+        answer++;
+    }
+    
+    return answer;
+}
